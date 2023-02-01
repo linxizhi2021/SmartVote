a12a5ad77be30adab17577e1a4f81a55b7c6bfa2
林之蛙 <97859420+linxizhi2021@users.noreply.github.com>
2023-02-01 21:21:44 +0800

    Update cache_helper.js

--- miniprogram/helper/cache_helper.js
+++ miniprogram/helper/cache_helper.js
@@ -1,7 +1,7 @@
 /**
  * Notes: 微信缓存二次封装，有设置时效性的封装
  * Ver : CCMiniCloud Framework 3.0.1 ALL RIGHTS RESERVED BY cclinux0730 (wechat)
- * Date: 2020-11-14 07:48:00 
+ * Date: 2022-11-14 07:48:00 
  */
 const helper = require('./helper.js');
 
